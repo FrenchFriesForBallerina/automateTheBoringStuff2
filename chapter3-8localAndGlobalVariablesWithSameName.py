ccfe67fb7288a9c0e082bef34f6679a9ed246ace
FriedBallerina <FriedBallerina@users.noreply.github.com>
2020-12-28 13:35:41 +0000

    Add files via upload

---
+++ chapter3-8localAndGlobalVariablesWithSameName.py
@@ -0,0 +1,13 @@
+def spam():
+    eggs = 'spam local'
+    print(eggs)
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs)
+    spam()
+    print(eggs)
+
+eggs = 'global eggs'
+bacon()
+print(eggs)
