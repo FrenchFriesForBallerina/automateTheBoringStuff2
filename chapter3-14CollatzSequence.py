80790a2ef2654c70992a7a1fb78a3aa49e19770d
FriedBallerina <FriedBallerina@users.noreply.github.com>
2021-01-01 21:25:04 +0000

    Add files via upload

--- chapter3-14CollatzSequence.py
+++ chapter3-14CollatzSequence.py
@@ -1,17 +1,20 @@
 def collatz(number):
 
     if(number % 2 == 0):
-        print(number // 2)
-        return number // 2
-    else:
-        print(3 * number + 1)
-        return (3 * number + 1)
+        value = number // 2
+        print(value)
+        return value
 
-userInput = int(input('Type in a number:\n'))
-result = collatz(userInput)
+    else:
+        value = 3 * number + 1
+        print(value)
+        return value
+
+userInput = input('Type in a number:\n')
+result = int(userInput)
+
 while True:
-    if collatz(result) != 1:
+    if result != 1:
         result = collatz(result)
     else:
-        print('the result is now 1')
         break
